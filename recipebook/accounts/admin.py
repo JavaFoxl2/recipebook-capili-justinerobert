4dcbccb3cfd3499134273a03b401c45349d15995
JavaFoxl2 <justine.capili@student.ateneo.edu>
2024-04-05 16:13:42 +0800

    changed models, admins and views

--- recipebook/accounts/admin.py
+++ recipebook/accounts/admin.py
@@ -8,10 +8,12 @@ from .models import Profile
 
 class ProfileInline(admin.StackedInline):
     model = Profile
-    can_delete=False
+    can_delete = False
 
-class UserAdmin (BaseUserAdmin):
-    inlines = [ProfileInline, ]
 
-admin.site.unregister (User)
-admin.site.register (User, UserAdmin)
+class UserAdmin(BaseUserAdmin):
+    inlines = [ProfileInline,]
+
+
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
